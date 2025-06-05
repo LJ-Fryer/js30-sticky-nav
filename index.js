3cf9e5bdced21d99537d62d697106ea77eaba22f
LJ-Fryer <ljf_aus@hotmail.com>
2025-06-05 16:38:54 +1000

    Completed

--- index.js
+++ index.js
@@ -1,1 +1,15 @@
-console.log("Testing");
+const nav = document.getElementById("main");
+const topOfNav = nav.offsetTop;
+
+function stickyNav() {
+  if (scrollY >= topOfNav) {
+    document.body.style.paddingTop = nav.offsetHeight + "px";
+    document.body.classList.add("fixed-nav");
+    console.log(nav.offsetHeight);
+  } else {
+    document.body.style.paddingTop = 0;
+    document.body.classList.remove("fixed-nav");
+  }
+}
+
+window.addEventListener("scroll", stickyNav);
